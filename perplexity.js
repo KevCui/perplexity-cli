70e8c5d57e6645baaed6eedafab8c883d68f243b
Kevin Cui <krazycavin@gmail.com>
2023-12-23 13:54:58 +0100

    Update script

--- perplexity.js
+++ perplexity.js
@@ -1,27 +1,31 @@
 #!/usr/bin/env node
 
 const { firefox } = require('playwright-firefox');
+
 const url = 'https://www.perplexity.ai/search?focus=internet&copilot=false&q=' + process.argv[2];
-const buttonMenu = 'button[data-testid="thread-dropdown-menu"]';
-const buttonDelete = 'div[data-testid="thread-delete"]';
-const buttonConfirm = 'css=button:has-text("Confirm")';
-const buttonCopy = 'svg[data-icon="clipboard"]';
+const buttonCopy = 'svg[data-icon="clipboard"]'; // copy icon on answer 
+const buttonMenu = 'button[data-testid="thread-dropdown-menu"]'; // "..." icon on top right
+const buttonDelete = 'div[data-testid="thread-delete"]'; // "Delete Thread" button
+const buttonConfirm = 'css=button:has-text("Confirm")'; 
 const textMessage = 'div[dir="auto"]';
 
 firefox.launch({ headless: true, timeout: 30000 }).then(async browser => {
+  // start session
   const page = await browser.newPage();
   await page.goto(url, { waitUntil: 'domcontentloaded' });
-  page.setDefaultTimeout(20000);
 
+  // get answer 
   await page.waitForSelector(buttonCopy);
   const result = await page.locator(textMessage).textContent();
   console.log(result);
 
+  // delete thread
   await page.click(buttonMenu, {focus: true});
   await page.waitForTimeout(300);
   await page.click(buttonDelete);
   await page.waitForTimeout(300);
   await page.click(buttonConfirm);
 
+  // close browser
   await browser.close();
 });
